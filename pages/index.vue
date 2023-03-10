07ecc43bc7202405c6357f01de36de125a251d75
Apiwan <gamebp02@hotmail.com>
2023-03-10 15:01:40 +0700

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,280 @@
+<template>
+
+<div class="relative overflow-x-auto">
+    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+        <thead class="text-xs text-gray-700 uppercase bg-gray-10 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+                <th scope="col" class="px-6 py-2">
+                    
+<img class="h-20 w-50 " src="https://www.adt.or.th/image/ADT1%20-%201108%20-%200263.jpg" alt="">
+บริษัท ศรีดาราทัวร์ จำกัด(สำนักงานใหญ่)
+<br><br>
+123 ถ.ชยางถูร ม.19 ต.บุ่ง อ.เมือง จ.อำนาจเจริญ 3700
+<br><br>
+เลขประจำตัวผู้เสียภาษี 03755520000037
+<br><br>
+เบอร์มือถือ 0823656514
+<br><br>
+wwww.facebook.com/sridaratourfanpage
+<br><br>
+<b>ลูกค้า</b><br>
+ธนาคารออมสินภาค 12(สำนักงานใหญ่)<br>
+343 ชั้น 3 หมู่ที่ 9 ตำบลโนนผึ้ง อำเภอวารินชำราบ จังหวัดอุบลราชธานี 34190<br>
+เลขประจำตัวผู้เสียภาษี 0994000164891<br>
+
+
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    <div align="center">
+                        
+    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">ใบกำกับภาษี / ใบเสร็จรับเงิน</h5>
+    
+    <h2 class="mb-2 text-1xl font-bold tracking-tight text-gray-900 dark:text-white">ต้นฉบับ(เอกสารออกเป็นชุด)</h2>
+    
+    <br><p class="font-normal text-gray-700 dark:text-gray-400">_____________________________________________________</p>
+    <br><p class="font-normal text-gray-700 dark:text-gray-400"><b>เลขที่&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;INV2023010001</b></p>
+    <p class="font-normal text-gray-700 dark:text-gray-400"><b>วันที่&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;31/10/2023</b></p>
+    <p class="font-normal text-gray-700 dark:text-gray-400"><b>ผู้ขาย&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Sasithorn Butta</b></p>
+    <br><p class="font-normal text-gray-700 dark:text-gray-400">_____________________________________________________</p>
+    <br><p class="font-normal text-gray-700 dark:text-gray-400"><b>ชื่องาน&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;ตั๋วเครื่องบินในประเทศ</b></p>
+    <p class="font-normal text-gray-700 dark:text-gray-400"><b>ผู้ติดต่อ&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;สำนักงานภาค</b></p>
+    <p class="font-normal text-gray-700 dark:text-gray-400"><b>เบอร์โทร&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;045424262</b></p>
+ 
+</div>
+                </th>
+            </tr>
+        </thead>
+        
+    </table>
+</div>
+<div class="overflow-x-auto">
+                <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                    <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400 ">
+                        <tr>
+                            <th scope="col" class="px-4 py-3">#</th>
+                            <th scope="col" class="px-4 py-3">รายละเอียด</th>
+                            <th scope="col" class="px-4 py-3">จำนวน</th>
+                            <th scope="col" class="px-4 py-3">ราคาต่อหน่วย</th>
+                            <th scope="col" class="px-4 py-3">ส่วนลด</th>
+                            <th scope="col" class="px-4 py-3">ภาษี</th>
+                            <th scope="col" class="px-4 py-3">มูลค่า</th>
+                            
+                            <th scope="col" class="px-4 py-3">
+                                <span class="sr-only">Actions</span>
+                              </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr class="border-b dark:border-gray-700">
+                            <th scope="row" class="px-4 py-3 font-medium text-gray-900 whitespace-nowrap dark:text-white">1</th>
+                            <td class="px-4 py-3">ค่าตั๋วเครื่องบิน<br>
+                            สายการบินแอร์นกแอร์<br>
+                            BOOKING : 71714783<br>
+                            วันที่ 2 กุมภาพันธ์ 66<br>
+                            อุบล-กทม.<br>
+                            14.30 น. - 15.30 น.<br>
+                            ตั๋วข้าราชการ<br>
+                            MRS.WARAPRON SAENSERM<br>
+                            </td>
+                            <td class="px-4 py-3">1</td>
+                            <td class="px-4 py-3">2,250.00</td>
+                            <td class="px-4 py-3"> </td>
+                            <td class="px-4 py-3">7%</td>
+                            <td class="px-4 py-3">2,250.00</td>
+                            
+                            
+                          
+                            <td class="px-4 py-3 flex items-center justify-end">
+                            </td>
+                            
+                        </tr>
+                        <th scope="row" class="px-4 py-3 font-medium text-gray-900 whitespace-nowrap dark:text-white">2</th>
+                            <td class="px-4 py-3">ภาษีสนามบิน<br>
+                            AIRPORT TAX<br>
+                            
+                            </td>
+                            <td class="px-4 py-3">1</td>
+                            <td class="px-4 py-3">50.00</td>
+                            <td class="px-4 py-3"> </td>
+                            <td class="px-4 py-3">0%</td>
+                            <td class="px-4 py-3">50.00</td>
+                            <td class="px-4 py-3 flex items-center justify-end">
+                            </td>
+                        
+                        
+                    </tbody>
+                   
+               
+                    <tbody>
+                        <tr class="border-b dark:border-gray-700">
+                            <th scope="row" class="px-4 py-3 font-medium text-gray-900 whitespace-nowrap dark:text-white">3</th>
+                            <td class="px-4 py-3">ค่าตั๋วเครื่องบิน<br>
+                            สายการบินแอร์นกแอร์<br>
+                            BOOKING : 71714783<br>
+                            วันที่ 6 กุมภาพันธ์ 66<br>
+                            กทม.-อุบล<br>
+                            11.30 น. - 12.35 น.<br>
+                            ตั๋วข้าราชการ<br>
+                            MRS.WARAPRON SAENSERM<br>
+                            </td>
+                            <td class="px-4 py-3">1</td>
+                            <td class="px-4 py-3">2,200.00</td>
+                            <td class="px-4 py-3"> </td>
+                            <td class="px-4 py-3">7%</td>
+                            <td class="px-4 py-3">2,200.00</td>
+                            
+                            
+                          
+                            <td class="px-4 py-3 flex items-center justify-end">
+                            </td>
+                            
+                        </tr>
+                        <th scope="row" class="px-4 py-3 font-medium text-gray-900 whitespace-nowrap dark:text-white">4</th>
+                            <td class="px-4 py-3">ภาษีสนามบิน<br>
+                            AIRPORT TAX<br>
+                            
+                            </td>
+                            <td class="px-4 py-3">1</td>
+                            <td class="px-4 py-3">100.00</td>
+                            <td class="px-4 py-3"> </td>
+                            <td class="px-4 py-3">0%</td>
+                            <td class="px-4 py-3">100.00</td>
+                            <td class="px-4 py-3 flex items-center justify-end">
+                            </td>
+                        
+                        
+                    </tbody>
+                   
+                </table>
+
+                </div>            
+
+<table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+              <th scope="col" class="px-6 py-2">   
+<br><br>
+<br><br><br><br><br><br>
+(สี่พันหกร้อยบาทถ้วน)</th>
+<th scope="col" class="px-6 py-3">
+ <div align="right">
+    <p class="font-normal text-gray-700 dark:text-gray-400"><b>รวมเป็นเงิน&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;4,600.00 บาท</b></p>
+    <br><p class="font-normal text-gray-700 dark:text-gray-400"><b>มูลค่าที่ไม่มี/ยกเว้นภาษี&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;150.00 บาท</b></p>
+    <br><p class="font-normal text-gray-700 dark:text-gray-400"><b>มูลค่าที่คำนวณภาษี &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;4,158.87 บาท</b></p>
+    <br><p class="font-normal text-gray-700 dark:text-gray-400"><b>ภาษีมูลค่าเพิ่ม 7% &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;291.13บาท</b></p>
+    <br><p class="font-normal text-gray-700 dark:text-gray-400"><b>จำนวนเงินรวทั้งสิ้น&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;4,600.00 บาท</b></p>
+</div>
+                </th>
+            </tr>
+        </thead>       
+    </table>
+<br><br><br><br>
+<div class="flex">
+  &nbsp;&nbsp;&nbsp;&nbsp;การชำระเงินจะสมบูรณ์เมื่อบริษัทได้รับเงินเรียบร้อยแล้ว &nbsp;&nbsp;&nbsp;
+    <div class="flex items-center mr-4">
+        <input id="inline-radio" type="radio" value="" name="inline-radio-group" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+        <label for="inline-radio" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">เงินสด</label>
+    </div>
+    <div class="flex items-center mr-4">
+        <input id="inline-2-radio" type="radio" value="" name="inline-radio-group" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+        <label for="inline-2-radio" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">เช็ค</label>
+    </div>
+    <div class="flex items-center mr-4">
+        <input checked id="inline-checked-radio" type="radio" value="" name="inline-radio-group" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+        <label for="inline-checked-radio" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">โอนเงิน</label>
+    </div>
+    <div class="flex items-center mr-4">
+        <input checked id="inline-checked-radio" type="radio" value="" name="inline-radio-group" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+        <label for="inline-checked-radio" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">บัตรเครดิต</label>
+    </div>
+</div>
+&nbsp;&nbsp;&nbsp;
+ธนาคาร...............................................................เลขที่.....................................................................วันที่.........................................จำนวนเงิน................................................
+
+<table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+              <th scope="col" class="px-6 py-2">   
+ในนาม ธนาคารออมสินภาค 12
+</th>
+<th scope="col" class="px-6 py-3">
+ <div align="right">
+   ในนาม บริษัท ศรีดาราทัวร์ จำกัด
+   </div>
+                </th>
+            </tr>
+        </thead>
+
+</table>
+
+<table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+              <th scope="col" class="px-6 py-2">   
+<div align="right">
+    
+    <br><br><br><br><br>
+   &nbsp;&nbsp;
+   ...................................................
+   &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+   &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+   .......................................................
+   &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+   
+   <br>
+   ผู้จ่ายเงิน
+   &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+   &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+   &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+   &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+   วันที่&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+   &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+   </div>
+</th>
+<center><img class="h-15 w-40 " src="https://www.adt.or.th/image/ADT1%20-%201108%20-%200263.jpg" alt=""></center>
+<th scope="col" class="px-6 py-3">
+ <div align="left">
+    
+     <img class="h-15 w-40 " src="https://www.gangbeauty.com/uppic/2017-12/14/3efada8b7.webp" alt="">
+  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+   &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+   &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+   &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+   &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+   &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+   31/01/2023<br>
+  
+   &nbsp;&nbsp;
+   ...................................................
+   &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+   &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+   .......................................................
+   
+   
+   <br>
+   &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+   ผู้รับเงิน
+   &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+   &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+   &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+  
+   วันที่
+   </div>
+                </th>
+            </tr>
+        </thead>
+
+</table>
+
+   
+</template>
+<script setup>
+const features = [
+  { name: 'Origin', description: 'Designed by Good Goods, Inc.' },
+  { name: 'Material', description: 'Solid walnut base with rare earth magnets and powder coated steel card cover' },
+  { name: 'Dimensions', description: '6.25" x 3.55" x 1.15"' },
+  { name: 'Finish', description: 'Hand sanded and finished with natural oil' },
+  { name: 'Includes', description: 'Wood card tray and 3 refill packs' },
+  { name: 'Considerations', description: 'Made from natural materials. Grain and color vary with each item.' },
+]
+</script>
